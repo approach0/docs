fe0ac6a10ae610c174977a2a8c8bdeede389f670
Wei <clock126@126.com>
2019-06-23 23:44:56 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # enable MathJax
+mathjax_path="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
 extensions = [
     'sphinx.ext.mathjax'
 ]
